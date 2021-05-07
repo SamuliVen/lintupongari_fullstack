8c7f504c1eab07734a54f911767744fd6fb1d2de
Samuli Vento <zamuli.vento@hotmail.com>
2021-05-07 13:17:36 +0300

    Login Key

--- controllers/login.js
+++ controllers/login.js
@@ -22,7 +22,7 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.KEY)
 
   response
     .status(200)
